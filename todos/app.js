5dc32154fd334c57d2503e898347d441120d5b50
Thomas Tan <thomasktan@gmail.com>
2021-04-06 13:08:55 +0800

    improved application console output

--- todos/app.js
+++ todos/app.js
@@ -4,6 +4,10 @@ const startFolder = __dirname + '/testdir';
 const searchTerm = 'TODO';
 
 findInFolder(startFolder, searchTerm).then((todoFiles) => {
+  console.clear();
+  console.log(
+    `The application found ${todoFiles.length} files that contain the TODO keyword:`
+  );
   console.log('todoFiles', todoFiles);
 });
 
